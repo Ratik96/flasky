7b518ae8f7df7000e19d3af24089df4c68eb9d09
Ratik <Ratik96@gmail.com>
2015-01-09 23:42:36 +0530

    Switched back to .sh and added virtualenv support

--- flask-simple.sh
+++ flask-simple.sh
@@ -1,6 +1,15 @@
 read -p "Application name: " app_name
 mkdir $app_name
 echo "========= Added main directory ========="
+read -p "Do you want to setup a virtualenv? (y/n)" venv
+if [ $venv = "y" ]; then
+   mkdir flask
+  cd flask
+  virtualenv .
+  pip install flask
+  cd ../
+  echo "========= Flask installed ========="
+fi
 cd $app_name
 touch runserver.py
 echo "========= Added runserver.py ========="
@@ -11,4 +20,3 @@ echo "========= __init__ and views.py added ========="
 mkdir static
 mkdir templates
 echo "========= Tasks completed successfully. Happy Coding! ========= "
-cd /..
