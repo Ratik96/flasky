5e2d353ef9e20cf06f4e3b7e583bb2fe194b722c
Ratik <Ratik96@gmail.com>
2015-01-09 23:58:54 +0530

    Added a basic index view automation and other wiring

--- flask-simple.sh
+++ flask-simple.sh
@@ -1,21 +1,25 @@
 read -p "Application name: " app_name
 mkdir $app_name
 echo "========= Added main directory ========="
-read -p "Do you want to setup a virtualenv? (y/n)" venv
+read -p "Do you want to setup a virtualenv? (y/n) " venv
 if [ $venv = "y" ]; then
    mkdir flask
   cd flask
   virtualenv .
+  source bin/activate
   pip install flask
   cd ../
   echo "========= Flask installed ========="
 fi
 cd $app_name
 touch runserver.py
+printf 'from %s import app' $app_name >> runserver.py
 echo "========= Added runserver.py ========="
 mkdir $app_name
 cd $app_name
 touch __init__.py views.py
+printf 'from flask import Flask\napp = Flask(__name__)\n\nfrom %s import views' $app_name>> __init__.py
+printf 'from %s import app\n\n@app.route("/"")\ndef index():\n\treturn "<h1><center>Hello World!</center></h1>"' $app_name>> views.py
 echo "========= __init__ and views.py added ========="
 mkdir static
 mkdir templates
